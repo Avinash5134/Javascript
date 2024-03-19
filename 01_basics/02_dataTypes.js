f797e8f3e3f1006124038424d87760e7b0e2ba22
Avinash5134 <123055181+Avinash5134@users.noreply.github.com>
2024-03-19 13:38:49 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";//this treats all JS code as newer version of javascript.
+
+// alert(3 + 3)//we are using nodejs, not browser so it will give error.
+
+console.log(3+3);
+
+console.log("hitesh")
+
+//tc 39 (ECMA) is the official javascripts documentations
+
+
+//DATATYPES:
+let name = "avinash"//String datatype
+let age = 20//number datatype
+let isLoggedIn = false//boolean datatype
+let state = null
+/*
+(1)number datatype range is 2 to power 53
+(2)bigint
+(3)string => ""
+(4)boolean
+=> true/false
+(5)null => standalone value
+(6)undefined => when not any value is assgned to a variable
+(7)symbol => for uniqueness
+(8)object
+*/
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof undefined);//datatype of undefined is undefined
+console.log(typeof null);//datatype of null is object
