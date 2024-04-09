2773807d31be645017d5edbc1e711fc6e9b43d22
Avinash5134 <123055181+Avinash5134@users.noreply.github.com>
2024-04-09 06:17:13 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+//#Primitive datatypes(7):
+/*
+// (1)String
+// (2)Number
+// (3)Boolean
+// (4)null
+// (5)undefined
+// (6)Symbol
+// (7)BigInt
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+*/
+
+//#Non-Primitive(Reference type): Arrays, Objects, Functions
+/*
+//Arrays:
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "hitesh",
+    age: 22,
+
+}
+
+//Functions:
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+console.log(typeof anotherId)
+*/
