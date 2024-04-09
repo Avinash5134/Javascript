001dd9adde969845b7a57b04928987587163a456
Avinash5134 <123055181+Avinash5134@users.noreply.github.com>
2024-04-09 05:54:07 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,8 +29,50 @@ console.log(booleanisLoggedIn);
 
 
 //(3)Converting into string:
+/*
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+
+//********************OPERATIONS************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)//{Addition}
+// console.log(2-2)//{Substraction}
+// console.log(2*2)//{Multiplication}
+// console.log(2**3)//{2 raised to the power 3}
+// console.log(2/3)//{2 divided by 3}
+// console.log(2%3)//{to find remainder when 2 is divided by 3}
+
+let str1 = "Hello"
+let str2 = "Hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);//IF sring is written first then the complete value will become a string
+console.log(1 + 2 + "2");//IF string is last, then first the operations will occur and then string part will be added to it in the end.
+
+console.log( (3 + 4) * 5 % 3);//Use parenthesis to solve these type of questions.
+
+/*The following below is wrong way of writing code:
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
+*/
