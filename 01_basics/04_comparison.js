bd51dd2adf3130e423dd49352550b820f2649430
Avinash5134 <123055181+Avinash5134@users.noreply.github.com>
2024-04-09 06:16:52 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);//Here null is changed to 0 so it returns true
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
